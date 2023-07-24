12f53c6d2c736546cf4cc7686ccf4f659cd8d206
灿烂 <1481086138@qq.com>
2023-07-24 15:57:56 +0800

    函数的基本使用

---
+++ 02_dart的函数使用/01_函数的基本使用.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  print(sum(10, 20));
+}
+
+// sum函数，接收两个参数，返回int类型
+// 返回的类型可以省略
+int sum(num1, num2) {
+  return num1 + num2;
+}
+// 如下：
+// sum(num1, num2) {
+//   return num1 + num2;
+// }
+
+
+
