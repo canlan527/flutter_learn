5916a1796115b61f1ef8767a07d82cf3995aa693
灿烂 <1481086138@qq.com>
2023-07-26 14:11:52 +0800

    赋值运算符

---
+++ 03_dart特殊运算符/01_赋值运算符.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  // ?? 当变量值为null时，会赋值给变量
+  // 1. ??=
+  // var name = null;
+  // name ??= 'why';
+  // print(name);
+
+  // 2. ?? 如果 name 值为 null，就把后面的值付给temp
+  // var name = null;
+  // var temp = name ?? 'jxz';
+  // print(temp);
+
+}
