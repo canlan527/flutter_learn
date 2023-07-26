99fbca5cb1047a2960dfb5d65f787ab3bd6cd488
灿烂 <1481086138@qq.com>
2023-07-26 14:25:40 +0800

    for循环

---
+++ 03_dart特殊运算符/03_for循环的使用.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // 1. 基础for循环
+  // for(var i = 0; i < 10; i ++) {
+  //   print(i);
+  // }
+
+  // 2.遍历数组
+   var list = ['coderwhy', 'hahah', '假面骑士w'];
+  //  for (var i in list) {
+  //    print(i);
+  //  }
+
+  // for (var i = 0; i < list.length; i++) {
+  //   print(list[i]);
+  // }
+}
