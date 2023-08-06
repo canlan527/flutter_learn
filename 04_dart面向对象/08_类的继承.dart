e21579829263d51938e19f2a91d32901dc62e66e
灿烂 <1481086138@qq.com>
2023-08-06 15:18:13 +0800

    类的继承

--- 04_dart面向对象/08_类的继承.dart
+++ 04_dart面向对象/08_类的继承.dart
@@ -1,22 +1,41 @@
-void main(List<String> args) {
-  
-}
-
-class Human {
+class Animal {
   String? name;
-  int? age;
-
-  Human(name, age);
+  int age;
+  Animal(this.name, this.age);
 }
 
-// 继承 extends
-// 继承属性 super
-class Male extends Human{
-  String sex = 'male';
-  Male(this.sex, String name, int age):super(name, age); 
+class Dog extends Animal {
+  String species;
+  Dog(super.name, super.age, {this.species = 'dog'});
+  run() {
+    print('dog running~');
+  }
 }
 
-class Female extends Human {
+class Break extends Dog {
   String sex = 'female';
-  Female(this.sex, String name, int age): super(name, age);
+  Break(super.name, super.age, [this.sex = 'female']);
+
+  bark() {
+    print('woof~ woof~');
+  }
+
+  // 重写Dog run 方法
+  @override
+  run() {
+    // TODO: implement run
+    super.run(); // 子类调用父类的方法
+    print('break running~');
+  }
+}
+
+void main(List<String> args) {
+  Dog d1 = Dog('mud', 5);
+  print('${d1.species} - ${d1.name} - ${d1.age}');
+
+  Break b = Break('break', 6);
+  print('${b.species} - ${b.name} - ${b.age} - ${b.sex}');
+
+  b..run()
+   ..bark();
 }
