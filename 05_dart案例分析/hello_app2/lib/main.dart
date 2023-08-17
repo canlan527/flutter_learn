255ae7e745c37fa3458934e97a95f8e66f471503
灿烂 <1481086138@qq.com>
2023-08-17 23:02:01 +0800

    语法修复

--- 05_dart案例分析/hello_app2/lib/main.dart
+++ 05_dart案例分析/hello_app2/lib/main.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('我的第一个flutter应用'),
+          title: const Text('我的第一个flutter应用'),
         ),
-        body: MyHomePage(),
+        body: const MyHomePage(),
       ),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
         child: Text(
       'Hello world',
       style: TextStyle(color: Colors.amber, fontSize: 50),
