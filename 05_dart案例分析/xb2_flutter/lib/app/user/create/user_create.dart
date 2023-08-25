36f61846ca18dd83be24aacc1e49c9789db66e2b
灿烂 <1481086138@qq.com>
2023-08-26 00:33:01 +0800

    使用表单Form部件统一验证表单域的值

--- 05_dart案例分析/xb2_flutter/lib/app/user/create/user_create.dart
+++ 05_dart案例分析/xb2_flutter/lib/app/user/create/user_create.dart
@@ -13,6 +13,7 @@ class _UserCreateState extends State<UserCreate> {
   // 给表单字段创建key，通过获取key可以获取表单内容，进行验证等功能
   final nameFieldKey = GlobalKey<FormFieldState>();
   final passwordFieldKey = GlobalKey<FormFieldState>();
+  final formKey = GlobalKey<FormState>(); //Form表单的key，可以统一管理form内的表单项的值
 
   // 文本编辑控制器：TextEditingController
   final nameFieldController = TextEditingController();
@@ -39,79 +40,85 @@ class _UserCreateState extends State<UserCreate> {
     });
 
     // 通过表单控制器设置初始值
-    nameFieldController.text = 'xiaonan';
-    passwordFildController.text = '123456';
+    // nameFieldController.text = 'xiaonan';
+    // passwordFildController.text = '123456';
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          '注册用户',
-          style: TextStyle(
-            fontWeight: FontWeight.w300,
-            fontSize: 32,
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            '注册用户',
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 32,
+            ),
           ),
-        ),
-        const SizedBox(height: 32,),
-        TextFormField(
-          key: nameFieldKey,
-          controller: nameFieldController,
-          decoration: const InputDecoration(
-            labelText: '用户名',
+          const SizedBox(height: 32,),
+          TextFormField(
+            key: nameFieldKey,
+            controller: nameFieldController,
+            decoration: const InputDecoration(
+              labelText: '用户名',
+            ),
+            onChanged: (value) => name = value,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) {
+              if(value == null || value.isEmpty) {
+                return '请输入用户名';
+              }
+              return null; // 返回null代表验证通过
+            },
           ),
-          onChanged: (value) => name = value,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          validator: (value) {
-            if(value == null || value.isEmpty) {
-              return '请输入用户名';
-            }
-            return null; // 返回null代表验证通过
-          },
-        ),
-        const SizedBox(height: 32,),
-         TextFormField(
-          key: passwordFieldKey,
-          controller: passwordFildController,
-          obscureText: true,
-          decoration: const InputDecoration(
-            labelText: '密码'
+          const SizedBox(height: 32,),
+           TextFormField(
+            key: passwordFieldKey,
+            controller: passwordFildController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: '密码'
+            ),
+            onChanged: (value) => password = value,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) {
+              if(value == null || value.isEmpty) {
+                return '请输入密码';
+              }
+              if(value.isNotEmpty && value.length < 6) {
+                return '请设置6位以上的密码';
+              }
+              return null; // 返回null代表验证通过
+    
+            },
           ),
-          onChanged: (value) => password = value,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          validator: (value) {
-            if(value == null || value.isEmpty) {
-              return '请输入密码';
-            }
-            if(value.isNotEmpty && value.length < 6) {
-              return '请设置6位以上的密码';
-            }
-            return null; // 返回null代表验证通过
+          const SizedBox(height: 32,),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+              minimumSize: const Size(double.infinity, 60),
+            ),
+            // 通过key获取表单内容
+            onPressed: () {
+              // 调用表单验证,每个表单项都逐一验证
+              // nameFieldKey.currentState!.validate();
+              // passwordFieldKey.currentState!.validate();
+              
+              // 使用form验证其包裹的表单项的内容
+              formKey.currentState!.validate();
 
-          },
-        ),
-        const SizedBox(height: 32,),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            textStyle: const TextStyle(fontSize: 20),
-            minimumSize: const Size(double.infinity, 60),
-          ),
-          // 通过key获取表单内容
-          onPressed: () {
-            // 调用表单验证
-            nameFieldKey.currentState!.validate();
-            passwordFieldKey.currentState!.validate();
-
-            print('用户名：${nameFieldKey.currentState!.value}, 密码：${passwordFieldKey.currentState!.value}');
-          },
-          // onPressed: () => print('用户名：$name, 密码：$password'),
-          child: const Text('注册用户')
-        )
-      ],
+              print('用户名：${nameFieldKey.currentState!.value}, 密码：${passwordFieldKey.currentState!.value}');
+            },
+            // onPressed: () => print('用户名：$name, 密码：$password'),
+            child: const Text('注册用户')
+          )
+        ],
+      ),
     );
   }
 }
