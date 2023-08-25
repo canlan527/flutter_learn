b2d84e6b326ba001fa29a22736a622376087a02a
灿烂 <1481086138@qq.com>
2023-08-26 01:15:09 +0800

    基础路由学习

--- 05_dart案例分析/xb2_flutter/lib/app/playground/playground.dart
+++ 05_dart案例分析/xb2_flutter/lib/app/playground/playground.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:xb2_flutter/app/playground/input/playground_input.dart';
-import 'package:xb2_flutter/app/playground/layout/playground_layout.dart';
+// import 'package:xb2_flutter/app/playground/input/playground_input.dart';
+// import 'package:xb2_flutter/app/playground/layout/playground_layout.dart';
+import 'package:xb2_flutter/app/playground/routing/playground_routing.dart';
 
 class Playground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PlaygroundInput();
+    return PlaygroundRouting();
   }
 }
