ce3e890f1517fddc92cec974ac2f805b60a59c56
灿烂 <1481086138@qq.com>
2023-08-25 20:57:34 +0800

    新增输入框组件获取value

--- 05_dart案例分析/xb2_flutter/lib/app/user/create/user_create.dart
+++ 05_dart案例分析/xb2_flutter/lib/app/user/create/user_create.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class UserCreate extends StatelessWidget {
+class UserCreate extends StatefulWidget {
+
+  @override
+  State<UserCreate> createState() => _UserCreateState();
+}
+
+class _UserCreateState extends State<UserCreate> {
+  String? name;
+  String? password;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,12 +24,27 @@ class UserCreate extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 32,),
+        TextField(
+          decoration: InputDecoration(
+            labelText: '用户名',
+          ),
+          onChanged: (value) => name = value,
+        ),
+        const SizedBox(height: 32,),
+         TextField(
+          obscureText: true,
+          decoration: const InputDecoration(
+            labelText: '密码'
+          ),
+          onChanged: (value) => password = value,
+        ),
+        const SizedBox(height: 32,),
         ElevatedButton(
           style: ElevatedButton.styleFrom(
             textStyle: const TextStyle(fontSize: 20),
             minimumSize: const Size(double.infinity, 60),
           ),
-          onPressed: () => print('注册用户'),
+          onPressed: () => print('用户名：$name, 密码：$password'),
           child: const Text('注册用户')
         )
       ],
