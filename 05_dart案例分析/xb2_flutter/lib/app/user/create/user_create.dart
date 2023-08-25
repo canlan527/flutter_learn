bc0e30cc055c34483e6818e548f1a16e32f84302
灿烂 <1481086138@qq.com>
2023-08-25 21:32:57 +0800

    通过key来获取表单字段

--- 05_dart案例分析/xb2_flutter/lib/app/user/create/user_create.dart
+++ 05_dart案例分析/xb2_flutter/lib/app/user/create/user_create.dart
@@ -10,6 +10,10 @@ class _UserCreateState extends State<UserCreate> {
   String? name;
   String? password;
 
+  // 给表单字段创建key，通过获取key可以获取表单内容，进行验证等功能
+  final nameFieldKey = GlobalKey<FormFieldState>();
+  final passwordFieldKey = GlobalKey<FormFieldState>();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,14 +28,16 @@ class _UserCreateState extends State<UserCreate> {
           ),
         ),
         const SizedBox(height: 32,),
-        TextField(
-          decoration: InputDecoration(
+        TextFormField(
+          key: nameFieldKey,
+          decoration: const InputDecoration(
             labelText: '用户名',
           ),
           onChanged: (value) => name = value,
         ),
         const SizedBox(height: 32,),
-         TextField(
+         TextFormField(
+          key: passwordFieldKey,
           obscureText: true,
           decoration: const InputDecoration(
             labelText: '密码'
@@ -44,7 +50,9 @@ class _UserCreateState extends State<UserCreate> {
             textStyle: const TextStyle(fontSize: 20),
             minimumSize: const Size(double.infinity, 60),
           ),
-          onPressed: () => print('用户名：$name, 密码：$password'),
+          // 通过key获取表单内容
+          onPressed: () => print('用户名：${nameFieldKey.currentState!.value}, 密码：${passwordFieldKey.currentState!.value}'),
+          // onPressed: () => print('用户名：$name, 密码：$password'),
           child: const Text('注册用户')
         )
       ],
