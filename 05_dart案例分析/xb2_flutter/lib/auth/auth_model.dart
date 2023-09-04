f5209f734d4fda5a5d86c757391379cdc68f4c69
灿烂 <1481086138@qq.com>
2023-09-04 22:41:07 +0800

    定义身份验证模型

--- 05_dart案例分析/xb2_flutter/lib/auth/auth_model.dart
+++ 05_dart案例分析/xb2_flutter/lib/auth/auth_model.dart
@@ -1,21 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:xb2_flutter/app/app_config.dart';
+import 'package:xb2_flutter/app/exceptions/http_exception.dart';
+import 'package:xb2_flutter/auth/auth.dart';
 
 // 定义状态类,继承ChangeNotifier，就可以订阅当前类的状态变化
 class AuthModel extends ChangeNotifier{
-  bool isLogined = false;
+  String userId = '';
   String name = '';
+  String token = '';
 
-  login() {
-    isLogined = true;
-    name = 'canlan';
-    print('请求登录');
-    notifyListeners();
+  bool get isLogined => token.isNotEmpty;
+
+  Future<Auth> login(LoginData data) async {
+    final uri = Uri.parse('${AppConfig.apiBaseUrl}/login');
+    final response = await http.post(uri, body: data.toJson());
+    final responseBody = jsonDecode(response.body);
+
+    if(response.statusCode == 200) {
+      final auth = Auth.fromJson(responseBody);
+
+      userId = auth.id.toString();
+      name = auth.name;
+      token = auth.token;
+      notifyListeners();
+      return auth;
+    } else {
+      throw HttpException(responseBody['message'] ?? '网络请求出了一点问题');
+    }
   }
 
   logout() {
-    isLogined = false;
+    userId = '';
     name = '';
-    print('退出登录');
+    token = '';
     notifyListeners();
   }
 }
