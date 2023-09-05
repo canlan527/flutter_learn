3211ebda051a839b883b4a7b7fd820e3aea286d0
灿烂 <1481086138@qq.com>
2023-09-05 22:32:55 +0800

    删除app.dart无关的引用

--- 05_dart案例分析/xb2_flutter/lib/app/app.dart
+++ 05_dart案例分析/xb2_flutter/lib/app/app.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:xb2_flutter/app/app_model.dart';
-import 'package:xb2_flutter/app/components/app_home.dart';
-import 'package:xb2_flutter/app/playground/layout/playground_layout.dart';
-import 'package:xb2_flutter/app/playground/routing/components/about.dart';
 import 'package:xb2_flutter/app/post/post_provider.dart';
-import 'package:xb2_flutter/app/post/show/post_show.dart';
-import 'package:xb2_flutter/app/post/show/post_show_model.dart';
 import 'package:xb2_flutter/app/router/app_route_information_parser.dart';
 import 'package:xb2_flutter/app/router/app_router_delegate.dart';
 import 'package:xb2_flutter/app/theme/app_theme.dart';
