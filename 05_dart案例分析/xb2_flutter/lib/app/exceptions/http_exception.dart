ac9441cbb7f4657f34b887dea1460ce0ae6f544a
灿烂 <1481086138@qq.com>
2023-09-08 23:36:39 +0800

    重新定义异常

--- 05_dart案例分析/xb2_flutter/lib/app/exceptions/http_exception.dart
+++ 05_dart案例分析/xb2_flutter/lib/app/exceptions/http_exception.dart
@@ -1,12 +1,5 @@
-class HttpException implements Exception {
-  late String message;
+import 'package:xb2_flutter/app/exceptions/app_exception.dart';
 
-  HttpException(String? message) {
-    this.message = message ?? '网络请求出了问题';
-  }
-
-  @override
-  String toString() {
-    return 'HttpException $message';
-  }
+class HttpException extends AppException {
+  HttpException([String message = '网络请求出了问题']);
 }
