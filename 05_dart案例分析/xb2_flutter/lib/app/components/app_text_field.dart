ef9bc2202963ab57446f5c8c43ce1dbef618025c
灿烂 <1481086138@qq.com>
2023-09-08 23:26:31 +0800

    改进 AppTextField 自定义小部件

--- 05_dart案例分析/xb2_flutter/lib/app/components/app_text_field.dart
+++ 05_dart案例分析/xb2_flutter/lib/app/components/app_text_field.dart
@@ -3,29 +3,50 @@ import 'package:flutter/material.dart';
 class AppTextField extends StatelessWidget {
   final String labelText;
   final ValueChanged<String>? onChanged;
+  // 扩展属性
+  final TextEditingController? controller;
+  final bool canValidate;
+  final bool isMultiline;
+  final bool enabled;
 
   AppTextField({
     required this.labelText,
     this.onChanged,
+    this.controller,
+    this.canValidate = true,
+    this.isMultiline = false,
+    this.enabled = true,
   });
 
   @override
   Widget build(BuildContext context) {
+    TextInputType? keyboardType;
+
+    if (isMultiline) {
+      keyboardType = TextInputType.multiline;
+    }
+
     return Container(
       padding: EdgeInsets.only(bottom: 32),
       child: TextFormField(
-            decoration: InputDecoration(
-              labelText: labelText,
-            ),
-            onChanged: onChanged,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            validator: (value) {
-              if(value == null || value.isEmpty) {
-                return '请输入$labelText';
-              }
-              return null; // 返回null代表验证通过
-            },
-          ),
+        controller: controller,
+        enabled: enabled,
+        keyboardType: keyboardType,
+        maxLength: isMultiline ? null : 1,
+        decoration: InputDecoration(
+          labelText: labelText,
+        ),
+        onChanged: onChanged,
+        autovalidateMode: canValidate
+            ? AutovalidateMode.onUserInteraction
+            : AutovalidateMode.disabled,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return '请输入$labelText';
+          }
+          return null; // 返回null代表验证通过
+        },
+      ),
     );
   }
 }
